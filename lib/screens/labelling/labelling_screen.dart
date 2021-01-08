ffcb522e623840743d4311afdcd499df0e09a6ab
Snahi <good.letterbox@gmail.com>
2021-01-08 21:15:32 +0100

    Incorporte image cropping library

--- lib/screens/labelling/labelling_screen.dart
+++ lib/screens/labelling/labelling_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-
+import 'package:image_cropper/image_cropper.dart';
 import 'package:flutter/material.dart';
 
 class LabellingScreen extends StatelessWidget {
@@ -10,11 +10,21 @@ class LabellingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    _crop();
     return Scaffold(
       appBar: AppBar(title: Text('Display the Picture')),
       // The image is stored as a file on the device. Use the `Image.file`
       // constructor with the given path to display the image.
       body: Image.file(File(imgPath)),
+    );
+  }
+
+  void _crop() async {
+    print(imgPath);
+    File croppedImage = await ImageCropper.cropImage(
+      sourcePath: imgPath,
+      maxWidth: 1080,
+      maxHeight: 1080,
     );
   }
 }
